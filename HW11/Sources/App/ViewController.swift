672ab42a6a61f1a43e391b88f0ddedaa5b1c8216
Kuanysh Auyelgazy <auyelgazy@gmail.com>
2023-02-23 22:49:45 +0600

    Initial commit.

---
+++ HW11/Sources/App/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  HW11
+//
+//  Created by Kuanysh al-Khattab Auyelgazy on 23.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Outlests
+
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Setup
+
+
+    // MARK: - Actions
+
+
+}
