9f12b48ad3b414c42d95d181753c8ae44d590d42
Kuanysh Auyelgazy <auyelgazy@gmail.com>
2023-02-24 14:48:39 +0600

    Set up project and added background.

--- HW11/Sources/App/ViewController.swift
+++ HW11/Sources/App/ViewController.swift
@@ -6,10 +6,64 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     // MARK: - Outlests
+
+    private let gradientLayer = CAGradientLayer()
+
+    private lazy var loginLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+
+    private lazy var loginTextField: UITextField = {
+        let textField = UITextField()
+        return textField
+    }()
+
+    private lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        return textField
+    }()
+
+    private lazy var loginButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+
+    private lazy var forgotPasswordButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+
+    /// Label "or connect with"
+    private lazy var additionalLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+
+    private lazy var facebookButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+
+    private lazy var twitterButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+
+    private lazy var noAccountLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+
+    private lazy var signUpButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
 
 
     // MARK: - Lifecycle
@@ -17,12 +71,50 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupGradientBackground()
+
     }
 
     // MARK: - Setup
 
+    private func setupGradientBackground() {
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [
+            UIColor.systemIndigo.cgColor,
+            UIColor.cyan.cgColor
+        ]
+        view.layer.addSublayer(gradientLayer)
+    }
+
+    private func setupHierarchy() {
+
+    }
+
+    private func setupLayout() {
+
+    }
+
 
     // MARK: - Actions
 
+    @objc private func loginPressed(){
+
+    }
+
+    @objc private func forgotPasswordPressed(){
+
+    }
+
+    @objc private func facebookPressed(){
+
+    }
+
+    @objc private func twitterPressed(){
+
+    }
+
+    @objc private func signUpPressed(){
+
+    }
 
 }
